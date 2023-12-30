f4ec14b06806cadf6cd5c9853414df5e9c4a2eed
Chestnykh Alex <chestnyh@i.ua>
2023-12-30 14:12:25 +0200

    Updated simple strategy

--- src/stratedies/simple-strategy/index.js
+++ src/stratedies/simple-strategy/index.js
@@ -2,8 +2,6 @@ const fs = require('node:fs');
 const { createLongPosition, createShortPosition } = require('../../api/create-order/index');
 const logger = require('../../logger');
 
-const fileToLog = `logs`
-
 class SimpleStrategy {
 
     #checkInterval = 60 * 1000;
@@ -20,50 +18,10 @@ class SimpleStrategy {
         this.#coinFollower = coinFollower;
     }
 
-    #isRising(prices) {
-        let raisingAmount = 0;
-        let previous = 0;
-        prices.forEach(price => {
-
-            price = parseFloat(price);
-
-            if (previous === 0) {
-                previous = price;
-                return;
-            }
-
-            if (price > previous) {
-                raisingAmount++;
-            }
-        });
-
-        if (raisingAmount >= 10) {
-            return true;
-        }
-    }
-
-    #isDowning(prices) {
-
-        let downingAmount = 0;
-        let previous = 0;
-        prices.forEach(price => {
-
-            price = parseFloat(price);
-
-            if (previous === 0) {
-                previous = price;
-                return;
-            }
-
-            if (price < previous) {
-                downingAmount++;
-            }
-        });
-
-        if (downingAmount >= 10) {
-            return true;
-        }
-
+    #resetPrices() {
+        this.#state.topPointPrice = 0;
+        this.#state.lowPointPrice = 0;
+        this.#state.currentPrice = 0;
     }
 
     run() {
@@ -93,21 +51,15 @@ class SimpleStrategy {
 
             if (this.#state.currentPrice - this.#state.lowPointPrice > 3000) {
                 const result = await createShortPosition({ symbol: 'BTCUSDT', quantity: 0.005 });
-                this.#state.topPointPrice = 0
-                this.#state.lowPointPrice = 0
-                this.#state.currentPrice = 0
+                this.#resetPrices();
                 logger.log(`SHORT POSITION CREATED = ${JSON.stringify(result)}`);
             }
             if (this.#state.currentPrice - this.#state.topPointPrice < -3000) {
                 const result = await createLongPosition({ symbol: 'BTCUSDT', quantity: 0.005 });
-                this.#state.topPointPrice = 0
-                this.#state.lowPointPrice = 0
-                this.#state.currentPrice = 0
+                this.#resetPrices();
                 logger.log(`LONG POSITION CREATED = ${JSON.stringify(result)}`);
             }
 
-            // If last 10 rising doing short
-            // If last 10 doening doing long
         }, this.#checkInterval);
     }
 
