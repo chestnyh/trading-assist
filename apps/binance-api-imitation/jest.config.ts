d1d625ee4a8dfee3df402fc3f02577087727fb8d
Oleksii Chestnykh <chestnyh198819@egmail.com>
2024-09-04 12:06:38 +0300

    Added binance-api-imitation

---
+++ apps/binance-api-imitation/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'binance-api-imitation',
+  preset: '../../jest.preset.cjs',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/binance-api-imitation',
+};
