53296924eb30d507fafbd2a4ce2e3f0f89f380cf
Oleksii Chestnykh <chestnyh198819@egmail.com>
2025-01-24 22:47:52 +0200

    Added some examples

--- apps/auto-trader/src/auto-trader/auto-trader.service.ts
+++ apps/auto-trader/src/auto-trader/auto-trader.service.ts
@@ -1,34 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ActionsRunnerService } from "../actions-runner/actions-runner.service";
 
-const config = {
-    "name": "Test simple config",
-    "description": "TODO add description",
-    "type": "parallel",
-    "arguments": {
-        "do": [
-            {
-                "type": "log",
-                "arguments": {
-                    "message": "Timeout message 1"
-                },
-            },
-            {
-                "type": "log",
-                "arguments": {
-                    "message": "Timeout message 2"
-                },
-            },
-            {
-                "type": "log",
-                "arguments": {
-                    "message": "Timeout message 3"
-                },
-            }
-        ]
-    }
-};
-
 @Injectable()
 export class AutoTraderService {
     constructor(
