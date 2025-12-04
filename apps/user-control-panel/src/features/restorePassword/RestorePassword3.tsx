a937722138e7bd66baa0b6439017de757c9273b0
Kseniia <kseniia.voitikh@gmail.com>
2025-12-04 21:16:48 +0200

    Added restore password step 3 layout

--- apps/user-control-panel/src/features/restorePassword/RestorePassword3.tsx
+++ apps/user-control-panel/src/features/restorePassword/RestorePassword3.tsx
@@ -1,7 +1,19 @@
+import { ManNextToTheSafe } from "./components/svg/ManNextToTheSafe";
+import { FormInput } from "../signInUp/components/FormInput";
+import { FormButton } from "../signInUp/components/FormButton";
+import { AuthLayout } from "../signInUp/components/AuthLayout";
+
 export function RestorePassword3() {
     return (
-        <div>
-            <h1>Restore Password 3</h1>
-        </div>
+        <AuthLayout
+            currentStep={2}
+            title="Enter new password"
+            Illustration={ManNextToTheSafe}
+            actions={<FormButton text="Set Up New Password" />}
+            totalSteps={3}
+        >
+            <FormInput label="Password" id="password" type="password" name="password" />
+            <FormInput label="Confirm Password" id="confirmPassword" type="password" name="confirmPassword" />
+        </AuthLayout>
     );
 }
